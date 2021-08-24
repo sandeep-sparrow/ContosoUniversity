0bf15915489eaa4e970bd953b703e16276a1ec6e
sandeep-sparrow <sandeep.p4856@gmail.com>
2021-08-25 00:55:59 +0530

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        // we will add properties of Student in Student.cs model
+        // ID will be come the primary key of Student table
+        public int ID { get; set; } 
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        // Enrollments property is a navigation property
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
